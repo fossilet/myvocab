b59eec56a23535fbf7ad0c451e19c1ff7336b11b
Yongzhi Pan <panyongzhi@gmail.com>
2014-07-21 10:41:10 +0800

    Fold extra links in a dropdown menu. close #8
    Also:
        Add Merriam-Webster and TheFreeDictionary.

--- functions.coffee
+++ functions.coffee
@@ -24,6 +24,24 @@ sites = [
     'link': 'http://etymonline.com/index.php?search=',
     'class': 'etymology'
   },
+]
+
+extra_sites = [
+  {
+    'name': 'Google Images',
+    'link': 'https://www.google.com/search?tbm=isch&q='
+    'class': 'googleimages'
+  },
+  {
+    'name': 'Merriam-Webster',
+    'link': 'http://www.merriam-webster.com/dictionary/',
+    'class': 'mw'
+  },
+  {
+    'name': 'The Free Dictionary',
+    'link': 'http://www.thefreedictionary.com/',
+    'class': 'tfd'
+  },
   {
     'name': 'Google Define',
     'link': 'https://www.google.com/search?q=define%3A+',
@@ -33,12 +51,7 @@ sites = [
     'name': 'Google Search',
     'link': 'https://www.google.com/search?q=',
     'class': 'googlesearch'
-  },
-  {
-    'name': 'Google Images',
-    'link': 'https://www.google.com/search?tbm=isch&q='
-    'class': 'googleimages'
-  },
+  }
 ]
 
 mangle = () ->
@@ -50,6 +63,7 @@ mangle = () ->
   # First remove added div then add it.
   ($ mydiv).remove()
   links = ("<a target='_blank' class='ext-link tbutton #{ site["class"]}' href='#{ site["link"] }#{ word }'>#{ site["name"] }</a>" for site in sites)
+  extra_links = ("<li class='submenu'><a target='_blank' class='ext-link tbutton #{ site["class"]}' href='#{ site["link"] }#{ word }'>#{ site["name"] }</a></li>" for site in extra_sites)
 
   if DEBUG
     console.log "word: #{ word }"
@@ -57,6 +71,19 @@ mangle = () ->
 
   ($ site_tools).after mydiv_content
   (($ mydiv).append link) for link in links
+
+  menu = """
+<ul class="menu">
+  <li class="menu">
+    More
+    <ul class="submenu">
+    </ul>
+  </li>
+</ul>
+"""
+
+  ($ mydiv).append menu
+  ($("#{ mydiv } ul li ul").append link) for link in extra_links
 
 DOMModificationHandler = ->
   # http://stackoverflow.com/q/11084819
--- functions.js
+++ functions.js
@@ -6,7 +6,7 @@ Created by tux, Sat Feb  8 23:48:59 CST 2014
  */
 
 (function() {
-  var DEBUG, DOMModificationHandler, container, mangle, sites;
+  var DEBUG, DOMModificationHandler, container, extra_sites, mangle, sites;
 
   DEBUG = true;
 
@@ -27,6 +27,22 @@ Created by tux, Sat Feb  8 23:48:59 CST 2014
       'name': 'Etymology',
       'link': 'http://etymonline.com/index.php?search=',
       'class': 'etymology'
+    }
+  ];
+
+  extra_sites = [
+    {
+      'name': 'Google Images',
+      'link': 'https://www.google.com/search?tbm=isch&q=',
+      'class': 'googleimages'
+    }, {
+      'name': 'Merriam-Webster',
+      'link': 'http://www.merriam-webster.com/dictionary/',
+      'class': 'mw'
+    }, {
+      'name': 'The Free Dictionary',
+      'link': 'http://www.thefreedictionary.com/',
+      'class': 'tfd'
     }, {
       'name': 'Google Define',
       'link': 'https://www.google.com/search?q=define%3A+',
@@ -35,15 +51,11 @@ Created by tux, Sat Feb  8 23:48:59 CST 2014
       'name': 'Google Search',
       'link': 'https://www.google.com/search?q=',
       'class': 'googlesearch'
-    }, {
-      'name': 'Google Images',
-      'link': 'https://www.google.com/search?tbm=isch&q=',
-      'class': 'googleimages'
     }
   ];
 
   mangle = function() {
-    var link, links, mydiv, mydiv_content, site, site_tools, word, _i, _len, _results;
+    var extra_links, link, links, menu, mydiv, mydiv_content, site, site_tools, word, _i, _j, _len, _len1, _results;
     word = $.trim($("" + container + " h1").text());
     site_tools = "" + container + " div.tools";
     mydiv_content = '<div class="ext-link"></div>';
@@ -58,15 +70,30 @@ Created by tux, Sat Feb  8 23:48:59 CST 2014
       }
       return _results;
     })();
+    extra_links = (function() {
+      var _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = extra_sites.length; _i < _len; _i++) {
+        site = extra_sites[_i];
+        _results.push("<li class='submenu'><a target='_blank' class='ext-link tbutton " + site["class"] + "' href='" + site["link"] + word + "'>" + site["name"] + "</a></li>");
+      }
+      return _results;
+    })();
     if (DEBUG) {
       console.log("word: " + word);
       console.log(links);
     }
     ($(site_tools)).after(mydiv_content);
-    _results = [];
     for (_i = 0, _len = links.length; _i < _len; _i++) {
       link = links[_i];
-      _results.push(($(mydiv)).append(link));
+      ($(mydiv)).append(link);
+    }
+    menu = "<ul class=\"menu\">\n  <li class=\"menu\">\n    More\n    <ul class=\"submenu\">\n    </ul>\n  </li>\n</ul>";
+    ($(mydiv)).append(menu);
+    _results = [];
+    for (_j = 0, _len1 = extra_links.length; _j < _len1; _j++) {
+      link = extra_links[_j];
+      _results.push($("" + mydiv + " ul li ul").append(link));
     }
     return _results;
   };
